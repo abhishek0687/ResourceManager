df1f88cf49316c520f15ecf38a097c185dfad53d
abhishek0687 <abhishek0687@gmail.com>
2019-06-22 17:16:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,72 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+var route = require('./routes/route');
+var dbConfig = require('./core/db');
+var db = dbConfig.dbs;
+//var initDb = db.initDb;
+//Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+
+
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    //dbConfig.initDb(function(err){});
+    console.log("Error connecting db");
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    //initDb(function(err){});
+    console.log("Error connecting db");
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
+
+
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+
+route.serve(app,express);
+module.exports = app ;
+  
